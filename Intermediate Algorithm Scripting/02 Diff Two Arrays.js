dbfcf2b2a781fc0ed8a47124f9f02ec2ea0653c1
William Silburn <25758160+WillGITCode@users.noreply.github.com>
2017-09-22 22:36:25 -0400

    Create 02 Diff Two Arrays.js

---
+++ Intermediate Algorithm Scripting/02 Diff Two Arrays.js
@@ -0,0 +1,32 @@
+/*
+Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+Comparison Operators
+Array.prototype.slice()
+Array.prototype.filter()
+Array.prototype.indexOf()
+Array.prototype.concat()
+*/
+
+function diffArray(arr1, arr2) {
+    var newArr = [];
+
+    for (var i = 0; i < arr1.length; i++) {
+        if (arr2.indexOf(arr1[i]) === -1) {
+            newArr.push(arr1[i]);
+        }
+    }
+    for (var k = 0; k < arr2.length; k++) {
+        if (arr1.indexOf(arr2[k]) === -1) {
+            newArr.push(arr2[k]);
+        }
+    }
+
+    return newArr;
+}
+
+diffArray([1, "calf", 3, "piglet"], [7, "filly"]);
