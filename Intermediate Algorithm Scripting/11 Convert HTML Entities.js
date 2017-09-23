5b7e60cb47852ab2fd6c2de89b057c8b7cb7c436
William Silburn <25758160+WillGITCode@users.noreply.github.com>
2017-09-22 22:41:08 -0400

    Create 11 Convert HTML Entities.js

---
+++ Intermediate Algorithm Scripting/11 Convert HTML Entities.js
@@ -0,0 +1,20 @@
+/*
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+RegExp
+HTML Entities
+String.prototype.replace()
+*/
+
+function convertHTML(str) {
+
+    var newStr = str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&apos;');
+
+    return newStr;
+}
+
+convertHTML("Hamburgers < Pizza < Tacos");
