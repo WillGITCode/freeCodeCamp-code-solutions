d1a56e396d049f8b1fc9efef121ee51a9529ab49
William S <25758160+WillGITCode@users.noreply.github.com>
2018-04-21 13:22:23 -0400

    Where do I belong latest solution. Not as good as first commit

--- Basic Algorithm Scripting/15 Where do I belong.js
+++ Basic Algorithm Scripting/15 Where do I belong.js
@@ -26,3 +26,30 @@ function compareNumbers(a, b) {
 }
 
 getIndexToIns([5, 3, 20, 3], 5);
+
+//latest try. I must have forgot how I solved it the first time. 
+
+function getIndexToIns(arr, num) {
+    var index = 0;
+    
+    arr.sort(function(a, b) {
+      return a - b;
+    });
+    
+    
+    for(var i = 0; i < arr.length; i++){
+      if(num > arr[i] && num < arr[i+1]){
+        index = i +1;
+        break;
+      }else if(num == arr[i] && num <= arr[i+1]){
+        index = i;
+        break;
+      }else if(num > arr[i] && i == arr.length-1){
+        index = i+1;
+      }
+    }
+    
+    return index;
+  }
+  
+  getIndexToIns([2, 5, 10], 15);
